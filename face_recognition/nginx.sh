5c7fea62e1cfc755dd77c880901242d7c6edbf11
hctung57 <manhtung05072001@gmail.com>
2023-03-16 14:55:44 +0700

    update chart

--- face_recognition/nginx.sh
+++ face_recognition/nginx.sh
@@ -4,9 +4,9 @@ echo "nginx: starting up nginx..."
 echo "nginx is running..."
 export SOURCE_RTMP_URL="$(getent hosts $SOURCE_STREAM_SERVICE | awk '{ print $1 ;exit }'):$SOURCE_RTMP_PORT"
 echo "exported url of source streaming: $SOURCE_RTMP_URL"
-echo "starting face detection func..."
+echo "starting face recognition func..."
 
-python3 face_detection.py -s $SOURCE_RTMP_URL
+python3 recognition.py -s $SOURCE_RTMP_URL -img $IMAGE_URL
 
 while true
 do :
@@ -16,8 +16,9 @@ then
     echo "Regenerate service ip address..."
     export SOURCE_RTMP_URL="$(getent hosts $SOURCE_STREAM_SERVICE | awk '{ print $1 ;exit }'):$SOURCE_RTMP_PORT"
     echo "exported url of source streaming: $SOURCE_RTMP_URL"
-    echo "Restarting face detection func..."
-    python3 face_detection.py -s $SOURCE_RTMP_URL
+    echo "Restarting face recognition func..."
+    python3 recognition.py -s $SOURCE_RTMP_URL -img $IMAGE_URL
+    
 fi
 sleep 1
 done
