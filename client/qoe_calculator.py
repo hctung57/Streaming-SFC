853b8ed5efd274f273d21e2c39aa1f75c09d37a1
hctung57 <manhtung05072001@gmail.com>
2023-06-01 14:22:48 +0700

    Update qoe calculator

--- client/qoe_calculator.py
+++ client/qoe_calculator.py
@@ -2,11 +2,11 @@ import Player
 
 
 # QoE arguments
-alpha = 1
-beta = 1.85
-gamma = 1
-theta = 0.5
-MIN_QOE = -1e4
+# alpha = 1
+# beta = 1.85
+# gamma = 1
+# theta = 0.5
+# MIN_QOE = -1e4
 
 # add I-frame and p-frame to a packet
 # qoe = 1 * quality / 1000 - 1.85 * rebuff/ 1000 - 1 * smooth / 1000
@@ -67,59 +67,59 @@ def pre_handle_data(buffer_data_file_name: str):
     return packet_data
 
 
-def get_smooth(data, downloaded_chunk_id, chunk_id):
-    # the fist packet
-    if downloaded_chunk_id == 0 and chunk_id == 0:
-        return 0
-    if downloaded_chunk_id < chunk_id:
-        return 0
-    else:
-        smooth = abs(data[chunk_id]['bitrate'] - data[chunk_id - 1]['bitrate'])
-        return smooth
+# def get_smooth(data, downloaded_chunk_id, chunk_id):
+#     # the fist packet
+#     if downloaded_chunk_id == 0 and chunk_id == 0:
+#         return 0
+#     if downloaded_chunk_id < chunk_id:
+#         return 0
+#     else:
+#         smooth = abs(data[chunk_id]['bitrate'] - data[chunk_id - 1]['bitrate'])
+#         return smooth
 
 
-def calculate_qoe(filename: str):
-    data = pre_handle_data(filename)
-    played_time = 0
-    downloaded_chunk_id = 1
-    number_of_chunk = len(data) - 1
-    t0 = data[0]['receive_complete_time']
-    QoE = 0
-    action_chunk_id = 0
-    while action_chunk_id <= number_of_chunk:
-        rebuf = 0
-        # check all packet downloaded
-        for packet in range(downloaded_chunk_id, number_of_chunk + 1):
-            if t0 + played_time >= data[packet]['receive_complete_time']:
-                # print(data[packet])
-                downloaded_chunk_id = packet
-        smooth = get_smooth(data=data, downloaded_chunk_id=downloaded_chunk_id, chunk_id=action_chunk_id)
-        buffer = data[action_chunk_id]['receive_complete_time'] - t0
-        # print("player is going to play packet", action_chunk_id, 'with timestamp: ', t0 + played_time)
-        # print("downloaded", downloaded_chunk_id, 'packet, received time',
-        #       data[action_chunk_id]['receive_complete_time'])
-        print("action chunk id fist",action_chunk_id)
-        print("downloaded chunk id second", downloaded_chunk_id)
-        if action_chunk_id <= number_of_chunk - 2:
-            if action_chunk_id <= downloaded_chunk_id - 1:
-                # play chunk with timestamp
-                played_time += data[action_chunk_id]['timestamp']
-                print("buffer", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time, t0 + played_time, data[downloaded_chunk_id]['receive_complete_time'])
-                action_chunk_id += 1
-            else:
-                downloaded_chunk_id += 1
-                print("**rebuffer:", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time,t0 + played_time,data[downloaded_chunk_id]['receive_complete_time'])
-                played_time += data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time
-        else:
-            if action_chunk_id <= downloaded_chunk_id:
-                played_time += data[action_chunk_id]['timestamp']
-                print("buffer", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time, t0 + played_time, data[downloaded_chunk_id]['receive_complete_time'])
-                action_chunk_id += 1
-            else:
-                downloaded_chunk_id += 1
-                print("**rebuffer:", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time,t0 + played_time,data[downloaded_chunk_id]['receive_complete_time'])
-                played_time += data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time
-        print(action_chunk_id,played_time)
+# def calculate_qoe(filename: str):
+#     data = pre_handle_data(filename)
+#     played_time = 0
+#     downloaded_chunk_id = 1
+#     number_of_chunk = len(data) - 1
+#     t0 = data[0]['receive_complete_time']
+#     QoE = 0
+#     action_chunk_id = 0
+#     while action_chunk_id <= number_of_chunk:
+#         rebuf = 0
+#         # check all packet downloaded
+#         for packet in range(downloaded_chunk_id, number_of_chunk + 1):
+#             if t0 + played_time >= data[packet]['receive_complete_time']:
+#                 # print(data[packet])
+#                 downloaded_chunk_id = packet
+#         smooth = get_smooth(data=data, downloaded_chunk_id=downloaded_chunk_id, chunk_id=action_chunk_id)
+#         buffer = data[action_chunk_id]['receive_complete_time'] - t0
+#         # print("player is going to play packet", action_chunk_id, 'with timestamp: ', t0 + played_time)
+#         # print("downloaded", downloaded_chunk_id, 'packet, received time',
+#         #       data[action_chunk_id]['receive_complete_time'])
+#         print("action chunk id fist",action_chunk_id)
+#         print("downloaded chunk id second", downloaded_chunk_id)
+#         if action_chunk_id <= number_of_chunk - 2:
+#             if action_chunk_id <= downloaded_chunk_id - 1:
+#                 # play chunk with timestamp
+#                 played_time += data[action_chunk_id]['timestamp']
+#                 print("buffer", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time, t0 + played_time, data[downloaded_chunk_id]['receive_complete_time'])
+#                 action_chunk_id += 1
+#             else:
+#                 downloaded_chunk_id += 1
+#                 print("**rebuffer:", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time,t0 + played_time,data[downloaded_chunk_id]['receive_complete_time'])
+#                 played_time += data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time
+#         else:
+#             if action_chunk_id <= downloaded_chunk_id:
+#                 played_time += data[action_chunk_id]['timestamp']
+#                 print("buffer", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time, t0 + played_time, data[downloaded_chunk_id]['receive_complete_time'])
+#                 action_chunk_id += 1
+#             else:
+#                 downloaded_chunk_id += 1
+#                 print("**rebuffer:", data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time,t0 + played_time,data[downloaded_chunk_id]['receive_complete_time'])
+#                 played_time += data[downloaded_chunk_id]['receive_complete_time'] - t0 - played_time
+#         print(action_chunk_id,played_time)
 
 
 # calculate_qoe("buffer.log")
--- client/rtmp_connect.py
+++ client/rtmp_connect.py
@@ -1,9 +1,24 @@
 import librtmp
 import logging
+import configargparse
 
 logging.basicConfig(filename='buffer.log', level=logging.INFO,
                     format='%(number_video_packet)s - %(send_request_time)s - %(start_receive_time)s - '
                            '%(receive_complete_time)s - %(timestamp)s - %(current_bitrate)s')
+
+
+def parser_args():
+    parser = configargparse.ArgParser(description="Capture delay NFV FIL HUST",
+                                      formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-s1", "--source_rtmp_1",
+                        help="source stream 1 path")
+    parser.add_argument("-name1", "--source_name_1",
+                        help="source stream 1 path")
+    parser.add_argument("-s2", "--source_rtmp_2",
+                        help="source stream 2 path")
+    parser.add_argument("-name2", "--source_name_2",
+                        help="source stream 2 path")
+    return parser.parse_args()
 
 
 def calculate_bitrate(packet_size: int, duration: int):
@@ -11,26 +26,43 @@ def calculate_bitrate(packet_size: int, duration: int):
     return bitrate
 
 
-conn = librtmp.RTMP("rtmp://localhost/live/stream", live=True)
-conn.connect()
-stream = conn.create_stream(update_buffer=True)
-number_video_packet = 0
-pre_packet_timestamp = 0
-current_bitrate = 0
-while conn.connected:
-    packet, send_request_time, start_receive_time, receive_complete_time = conn.read_packet()
-    if packet:
-        if packet.channel == 7:
-            if number_video_packet != 0:
-                current_duration = packet.timestamp - pre_packet_timestamp
-                current_bitrate = calculate_bitrate(
-                    packet_size=packet.packet.m_nBodySize, duration=current_duration)
-                pre_packet_timestamp = packet.timestamp
-            number_video_packet += 1
-            logging.info('', extra={
-                'number_video_packet': number_video_packet, 'send_request_time': send_request_time,
-                'start_receive_time': start_receive_time, 'receive_complete_time': receive_complete_time,
-                'timestamp': packet.timestamp, 'current_bitrate': current_bitrate, })
-    else:
-        print("End of stream")
-        break
+# parser init
+args = parser_args()
+source_rtmp_1 = args.source_rtmp_1
+source_name_1 = args.source_name_1
+source_rtmp_2 = args.source_rtmp_2
+source_name_2 = args.source_name_2
+url_1 = f'rtmp://{source_rtmp_1}/live/stream'
+url_2 = f'rtmp://{source_rtmp_2}/live/stream'
+
+
+def rtmp_connect(url: str, file_name: str):
+    file_name = 'packet' + file_name + '.log'
+    logging.basicConfig(filename=file_name, level=logging.INFO,
+                        format='%(number_video_packet)s - %(send_request_time)s - %(start_receive_time)s - '
+                               '%(receive_complete_time)s - %(timestamp)s - %(current_bitrate)s')
+    conn = librtmp.RTMP(url, live=True)
+    conn.connect()
+    stream = conn.create_stream(update_buffer=True)
+    number_video_packet = 0
+    pre_packet_timestamp = 0
+    current_bitrate = 0
+    print("Start capture packet")
+    while conn.connected:
+        packet, send_request_time, start_receive_time, receive_complete_time = conn.read_packet()
+        if packet:
+            if packet.channel == 7:
+                if number_video_packet != 0:
+                    current_duration = packet.timestamp - pre_packet_timestamp
+                    print("Packet size:", packet.packet.m_nBodySize)
+                    current_bitrate = calculate_bitrate(
+                        packet_size=packet.packet.m_nBodySize, duration=current_duration)
+                    pre_packet_timestamp = packet.timestamp
+                number_video_packet += 1
+                logging.info('', extra={
+                    'number_video_packet': number_video_packet, 'send_request_time': send_request_time,
+                    'start_receive_time': start_receive_time, 'receive_complete_time': receive_complete_time,
+                    'timestamp': packet.timestamp, 'current_bitrate': current_bitrate, })
+        else:
+            print("End of stream")
+            break
